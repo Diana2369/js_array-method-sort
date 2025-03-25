a6cfa2077b37b5a8ba13974008bff5c58c001aad
Diana <dinamitka2369@gmail.com>
2025-03-25 22:35:38 +0100

    sort2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,14 +4,33 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  const originalSort = [].__proto__.sort;
-
   [].__proto__.sort2 = function (compareFunction) {
-    if (typeof compareFunction === 'function') {
-      return originalSort.call(this, compareFunction);
-    }
+    const compare =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) =>
+            String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0;
 
-    return originalSort.call(this);
+    const array = this;
+    let len = array.length;
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 0; i < len - 1; i++) {
+        if (compare(array[i], array[i + 1]) > 0) {
+          const temp = array[i];
+
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+          swapped = true;
+        }
+      }
+      len--;
+    } while (swapped);
+
+    return array;
   };
 }
 
